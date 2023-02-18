69db77603350d117506a5939a81583177f2b9c7b
Sangh0 <hop7311@naver.com>
2023-02-18 22:12:07 +0900

    refactor: add a paper vision

--- detection/qfocal.py
+++ detection/qfocal.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# A version similiary to General Focal Loss
 class QualityFocalLoss(nn.Module):
     """
     Args:
@@ -25,7 +25,7 @@ class QualityFocalLoss(nn.Module):
         >>> loss = loss_func(outputs, labels)
         >>> loss.backward()
     """
-    def __init__(self, pos_weight: torch.Tensor, gamma=1.5, alpha=0.25, reduction='none', use_sigmoid=True):
+    def __init__(self, pos_weight: torch.Tensor, gamma=1.5, alpha=0.25, reduction='mean', use_sigmoid=True):
         super(QFocalLoss, self).__init__()
         assert reduction in ('mean', 'sum', 'none')
         self.loss_func = nn.BCEWithLogitsLoss(pos_weight=pos_weight) \
@@ -48,3 +48,72 @@ class QualityFocalLoss(nn.Module):
             return loss.sum()
         else:
             return loss
+
+
+# Quality Focal Loss to consider IoU scores
+def quality_focal_loss(
+    logits, 
+    labels, 
+    score, 
+    beta: float=2.0,
+    weight: Optional[torch.Tensor]=None,
+):
+    logits_prob = logits.sigmoid()
+    scale_factor = logits_prob
+    zerolabel = scale_factor.new_zeros(logits.shape)
+    loss = F.binary_cross_entropy_with_logits(
+        logits, zerolabel, reduction='none') * scale_factor.pow(beta)
+
+    bg_class_ind = logits.size(1)
+    pos = ((label >= 0) & (label < bg_class_ind)).nonzero().squeeze(1)
+    pos_label = labels[pos].long()
+
+    scale_factor = score[pos] - logits_prob[pos, pos_label]
+    loss[pos, pos_label] = F.binary_cross_entropy_with_logits(
+        logits[pos, pos_label], score[pos], reduction='none') * scale_factor.abs().pos(beta)
+    
+    loss = loss * weight if weight is not None else loss
+    loss = loss.sum(dim=1, keepdim=False)
+    return loss
+
+
+class QualityFocalLoss(nn.Module):
+    """
+    Args:
+        use_sigmoid: whether to use sigmoid activation function
+            must set True
+        beta: a hyperparameter for focusing the easy examples
+        reduction: the output type that can be selected from none, sum and mean
+        weight: a weight tensor for each class
+    Forward:
+        logits: a prediction with torch tensor type and has shape of (N, ...)
+        labels: a label with torch tensor type and has shape of (M, ...)
+    Examples:
+        >>> loss_func = QualityFocalLoss()
+        >>> outputs = model(images)
+        >>> loss = loss_func(outputs, labels)
+        >>> loss.backward()
+    """
+    def __init__(
+        self, 
+        use_sigmoid: bool=True, 
+        beta: float=2.0, 
+        reduction='mean', 
+        weight: Optional[torch.Tensor]=None,
+    ):
+        super(QualityFocalLoss, self).__init__()
+        assert reduction in ('mean', 'sum', 'none')
+        assert use_sigmoid is True
+        self.reduction = reduction
+        self.use_sigmoid = use_sigmoid
+        self.beta = beta
+        self.weight = weight
+
+    def forward(self, logits: torch.Tensor, labels: torch.Tensor, score: torch.Tensor):
+        if self.use_sigmoid:
+            loss = quality_focal_loss(
+                logits, labels, score, self.beta, self.weight,
+            )
+        else:
+            raise NotImplementedError
+        return loss
